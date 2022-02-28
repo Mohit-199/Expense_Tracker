13eaabd44b1104c8990bf7162f4b0ed17823d1d0
Mohit <mohitkapoor38970@gmail.com>
2022-03-01 01:11:14 +0530

    expenseContent variable is used and env variables are changed on netlify

--- src/components/ExpenseList/ExpenseList.js
+++ src/components/ExpenseList/ExpenseList.js
@@ -5,7 +5,7 @@ import "./ExpenseList.css"
 const ExpenseList = (props) => {
         let expenseContent = <p>No expense found.</p>;
         if(props.items.length===0){
-            return <h2 className="expenses-list__fallback">Found no expenses.</h2>;
+            return <h2 className="expenses-list__fallback">{expenseContent}</h2>;
         } 
         
     return <ul className="expenses-list">
